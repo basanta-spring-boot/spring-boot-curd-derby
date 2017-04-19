d9dc3b671de9c19a260375d7bd9ebcb658d32ead
basahota <basahota@users.noreply.github.com>
2017-04-20 01:16:19 +0530

    Uploaded standard application

---
+++ src/main/java/com/spring/boot/curd/app/EmsApiEmbadedDbApplication.java
@@ -0,0 +1,12 @@
+package com.spring.boot.curd.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsApiEmbadedDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsApiEmbadedDbApplication.class, args);
+	}
+}
